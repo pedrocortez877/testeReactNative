f4cc3821d6541d53e4f25951146d0b14e3c341f6
Pedro Cortez <pedrocortez877@gmail.com>
2022-05-20 19:08:24 -0300

    feat: corrigindo cor da statusBar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import Routes from './src/routes';
 
+import colors from './src/styles/colors';
+
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.background} />
+      <Routes />
+    </>
+  );
 }
